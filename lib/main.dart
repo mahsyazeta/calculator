63c14113f46e324e17d23409667f7f27754f2fa0
mahsyazeta <zetadhana@gmail.com>
2022-03-01 12:00:35 +0700

    update error

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,98 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  // ignore: dead_code
+  _MyAppState createState()=>_MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController masukanPertama = TextEditingController();
+  TextEditingController masukanKedua = TextEditingController();
+  int? hasil =0, input1 =0, input2=0;
+
+  tambah(){
+    setState(() {
+      input1 = int.parse(masukanPertama.text);
+      input2 = int.parse(masukanKedua.text);
+      hasil = input1!+input2!;
+    });
+  }
+
+  kurang(){
+    setState(() {
+      input1 = int.parse(masukanPertama.text);
+      input2 = int.parse(masukanKedua.text);
+      hasil = input1!-input2!;
+    });
+  }
+
+  kali(){
+    setState(() {
+      input1 = int.parse(masukanPertama.text);
+      input2 = int.parse(masukanKedua.text);
+      hasil = input1!*input2!;
+    });
+  }
+
+  bagi(){
+    setState(() {
+      input1 = int.parse(masukanPertama.text);
+      input2 = int.parse(masukanKedua.text);
+      hasil = input1!~/input2!;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Prestest',
-      theme: ThemeData(primarySwatch: Colors.pink),
-      home: Scaffold(appBar: AppBar(title: Text('Kalkulator')),
-        body: Column(children: [
+    return Scaffold(
+       body: Column(children: [
           //text
-          Container(
-            child: Row(
-              children: [
-                Text('Hasil Perhitungan : '),
-              ],
-            ),
+          Row(
+            children: [
+              Text('Hasil Perhitungan : $hasil'),
+            ],
           ),
           // text form 1
-          Container(
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "masukan nomor pertama" ),
-            ),
+          TextField(
+            controller: masukanPertama,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "masukan nomor pertama" ),
           ),
-          Container(
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "masukan nomor kedua" ),
-            ),
+          //text form 2
+          TextField(
+            controller: masukanKedua,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "masukan nomor kedua" ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: (){tambah();}, child: const Text('+')),
+              ElevatedButton(onPressed: (){kurang();}, child: const Text('-'))
+              
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: (){kali();}, child: const Text('*')),
+              ElevatedButton(onPressed: (){bagi();}, child: const Text('/'))
+              
+            ],
           )
         ]),
-      ) 
       );
   }
+  
 }
 
